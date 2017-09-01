4c95d93f8302d7f1e08718958f0b962428afe0e4
cso <cso@cin.ufpe.br>
2017-09-01 01:47:58 -0300

    Acess to mysql server

--- news_portal/app/routes/news.js
+++ news_portal/app/routes/news.js
@@ -1,5 +1,18 @@
 module.exports = function(app){
 	app.get('/news', function(request, response){
-		response.render("news/news");
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'cristiano',
+			password : 'cristiano',
+			database : 'portal_noticias'
+		});
+
+		connection.query('select * from noticias', function(error, result){
+			response.render("news/news", {noticias : result});
+		});
 	});
+	// app.get('/news', function(request, response){
+	// 	response.render("news/news");
+	// });
 }
