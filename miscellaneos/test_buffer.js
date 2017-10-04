684a388e1814ad077d28683d4a9c17facd2f6b53
cso <cso@cin.ufpe.br>
2017-10-04 01:46:18 -0300

    Stream test - nodejs basics concepts

--- miscellaneos/test_buffer.js
+++ miscellaneos/test_buffer.js
@@ -3,3 +3,13 @@ len = buf.write("Elon Musk gonna built a civilization.");
 
 console.log("Octets written : "+ len);
 console.log(buf.toString('utf8', 0, len));
+
+//Transforming to json format
+// var json = buf.toJSON(buf);
+// console.log(json);
+
+var buf1 = new Buffer("Cristiano ");
+var buf2 = new Buffer("Oliveira.");
+var buf3 = Buffer.concat([buf1,buf2]);
+console.log('Buf3 content: '+buf3.toString());
+
