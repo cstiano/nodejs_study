cf42808e7847efda3bec2d2d74e378791251acca
cso <cso@cin.ufpe.br>
2017-10-05 03:28:03 -0300

    modification

--- miscellaneos/RESTful/post_mongo.js
+++ miscellaneos/RESTful/post_mongo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var Schema = mongoose.Schema;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend:true}));
@@ -11,9 +11,13 @@ app.post('/testPost', function(request,response){
 	var db = mongoose.connection;
 	db.on('error', console.error);
 	db.once('open', function(){
-		var busSchema = mongoose.Schema({
-			placa: String,
-			id_sensor: Number,
+		var busSchema = new Schema({
+			device : Schema.Types.Mixed,
+			data : Schema.Types.Mixed,
+			temp : Schema.Types.Mixed,
+			hum: Schema.Types.Mixed,
+			lum: Schema.Types.Mixed,
+			voltage: Schema.Types.Mixed
 		});
 
 		var Bus = mongoose.model('Bus', busSchema);
@@ -24,7 +28,7 @@ app.post('/testPost', function(request,response){
 			else console.log("Saved on database.");
 		});
 	});
-	mongoose.connect('mongodb://mobbipp_app:mobbipp0@ds161164.mlab.com:61164/mobbipp_db/fleet');
+	mongoose.connect('mongodb://mobbipp_app:mobbipp0@ds161164.mlab.com:61164/mobbipp_db');
 	response.end("Finished");
 });
 
